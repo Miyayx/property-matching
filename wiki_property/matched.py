5766b77bad54dcdbc319645e718ea4ba551f61ff
Miyayx <yangxue00.yxmn@gmail.com>
2015-04-22 21:47:21 +0800

    match dump info and crawled info in single language

---
+++ wiki_property/matched.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# -*- utf-8 -*-
+
+from models import *
+from utils import *
+
+EN_ZH_CL_FILE = '/home/keg/data/wikiraw/'
+ENWIKI_DUMP_INFOBOX = '/home/keg/data/wikiraw/enwiki-infobox-tmp.dat'
+ZHWIKI_DUMP_INFOBOX = '/home/keg/data/wikiraw/zhwiki-infobox-tmp.dat'
+ENWIKI_CRAWL_INFOBOX = '/home/keg/data/infobox/enwiki-infobox-scrapy.dat'
+ZHWIKI_CRAWL_INFOBOX = '/home/keg/data/infobox/zhwiki-infobox-scrapy.dat'
+
+def read_en_zh_cl(fn):
+    return [line.strip('\n').split('\t\t') for line in open(fn)]
+    
+def read_wiki_dump(infoboxes, fn):
+    for line in open(fn):
+        t, inf = line.strip('\n').split('\t\t')
+        if t in infoboxes:
+            props = [DumpProperty(p.split('::::=')) for p in inf.split('::::')]
+            infoboxes[t].dump_props = props
+
+def read_wiki_page(infoboxes, fn):
+    for line in open(fn):
+        t, inf = line.strip('\n').split('\t\t')
+        if t in infoboxes:
+            props = []
+            for p in inf.split(':::'):
+                prop, v = p.split('::=')
+                if '[[' in prop:
+                    if '|' in prop:
+                        ll, pl = prop[2:-2].split('|') #有链接的话第一个是link，第二个是label
+                    else:
+                        ll, pl = prop[2:-2] #有链接，两个label一样
+                else:
+                    pl = prop #没有链接，文本就是prop_label
+                    ll = ''
+            props.append(PageProperty(pl, ll ,v))
+            infoboxes[t].page_props = props
+
+def combine_prop(infoboxes):
+    """
+    默认一个article下的dumpproperty集合一定包含pageproperty
+    即pageproperty在dumpproperty有且只有一个对应
+    
+    """
+    for infobox in infoboxes.values():
+        pps = infobox.page_props
+        dps = infobox.dump_props
+        props = []
+        for pp in pps:
+            matched = False
+            for dp in dps:
+                if pp.prop_label == dp.dump_label:
+                    props.append(Property(pp.prop_label, pp.link_label, dp.dump_label, dp.value)) #保留dump中的value，信息比较正确、完整
+                    matched = True
+                    break
+
+            if not matched:
+                t1 = pp.value
+                v1 = text_to_vector(t1)
+                mdp = None
+                m = 0
+                
+                for dp in dps:
+                    t2 = dp.value
+                    v2 = text_to_vector(t2)
+                    cosine = get_cosine(vector1, vector2)
+                    if cosine > m:
+                        m = cosine
+                        mdp = dp
+                props.append(Property(pp.prop_label, pp.link_label, mdp.dump_label, mdp.value)) #保留dump中的value，信息比较正确、完整
+        infobox.props = props
+        del infobox.dump_props
+        del infobox.page_props
+        infoboxes[infobox.title] = infobox
+
+    return infoboxes
+
+
+def dump_stat(infoboxes):
+    props = set()
+    for infobox in infoboxes:
+        for dp in infobox.dump_props:
+            props.add(dp.dump_label)
+    return len(props)
+
+def page_stat(infoboxes):
+    props = set()
+    for infobox in infoboxes:
+        for dp in infobox.page_props:
+            props.add(dp.prop_label)
+    return len(props)
+
+def main():
+    en_zh = read_en_zh_cl(EN_ZH_CL_FILE)
+    infoboxes = {}
+    for en, zh in en_zh:
+        infoboxes[zh] = Infobox(zh)
+    read_wiki_dump(infoboxes, ZHWIKI_DUMP_INFOBOX)
+    read_wiki_page(infoboxes, ZHWIKI_CRAWL_INFOBOX)
+
+    dnum = dump_stat(infoboxes)
+    pnum = page_stat(infoboxes)
+
+    combine_prop(infoboxes)
+    
+        
---
+++ wiki_property/models.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+class PageProperty:
+    
+    prop_label = ''  #属性名称，即显示在网页上的文本
+    link_label = ''  #如果在网页中有链接的话，链接指向的article label
+    value = ''  #网页中爬取的value值
+
+    def __init__(self, pl, ll, v):
+        self.prop_label = pl
+        self.link_label = ll
+        self.value = v
+
+class DumpProperty:
+    dump_label = ''  #dump文件中的属性名称，可能是template属性
+    value = ''  #dump文件中的value值
+    
+    def __init__(self, l, v):
+        self.dump_label = l
+        self.value = v
+
+class Property:
+    """
+    PageProperty和DumpProperty对齐后的产物，只保留page上有的，不保留dump多出来的property
+    """
+    prop_label = ''  #属性名称，即显示在网页上的文本
+    link_label = ''  #如果在网页中有链接的话，链接指向的article label
+    dump_label = ''  #dump文件中的属性名称，可能是template属性
+    value = ''       #dump文件中的value值, 爬取的value值只是用来对应同语言下的property的，之后就没有用了
+
+    def __init__(self, pl, ll, dl, v):
+        self.prop_label = pl
+        self.link_label = ll
+        self.dump_label = dl
+        self.value = v
+
+class Infobox:
+
+    title = '' # article title
+    props = [] 
+
+    def __init__(self, t):
+        self.title = t
+
+
+class MatchedInfobox:
+
+    title = {'en':'', 'zh':''}
+    matched_props = [] 
+    en_props = []  # Property List
+    zh_props = []  # Property List
+
+
