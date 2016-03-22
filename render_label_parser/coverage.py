20015b5da85e3d4260d16d34545414816b013029
Miyayx <yangxue00.yxmn@gmail.com>
2016-03-22 20:07:11 +0800

    coverage

---
+++ render_label_parser/coverage.py
@@ -0,0 +1,117 @@
+# -*- coding:utf-8 -*-
+
+import codecs
+import sys
+import copy
+sys.path.append('..')
+from utils.logger import *
+initialize_logger('./coverage.log')
+
+def read_template_triple(fn):
+    print "Reading %s ..."%fn
+    d = {}
+    for line in codecs.open(fn, 'r', 'utf-8'):
+        tem, tl, rl = line.strip('\n').split('\t')
+        if not tem in d:
+            d[tem] = {}
+        d[tem][tl] = rl
+
+    return d
+
+def read_redirect_template(fn):
+    print "Reading %s ..."%fn
+    return dict(line.strip('\n').split('\t') for line in codecs.open(fn, 'r', 'utf-8'))
+
+def read_wiki_infobox(fn):
+    print "Reading %s ..."%fn
+    a_tem = {}
+    a_infobox = {}
+    for line in codecs.open(fn, 'r', 'utf-8'):
+        if not '\t\t' in line:
+            continue
+        title, info = line.strip('\n').split('\t\t')
+        info = info.split('\t')[0]
+        tem, infobox = info.split(':::::',1)
+        tem = 'Template:'+tem
+
+        a_tem[title] = tem
+        
+        for pair in infobox.split('::::;'):
+            try:
+                p, v = pair.split('::::=')
+            except:
+                print title, tem, pair
+                continue
+            if not title in a_infobox:
+                a_infobox[title] = {}
+            else:
+                a_infobox[title][p] = v
+    return a_tem, a_infobox
+
+def template_replace(a_tem, tem_redirect, tems):
+    """
+    用标准template替代抽取出的不规则template命名
+    """
+    a_tem2 = copy.copy(a_tem)
+    for a, tem in a_tem2.iteritems():
+        ntem = tem.strip().replace('_',' ')
+        if ntem in tem_redirect:
+            ntem = tem_redirect[ntem]
+        if ntem in tems:
+            a_tem[a] = ntem  #修改一下抽取出来的不规则的template命名
+            print tem,'-->',ntem
+    return a_tem
+
+def template_coverage(tem_triple, a_tem, tem_redirect):
+    count = 0
+    tems = tem_triple.keys()
+    fw = codecs.open('not_cover_template.dat', 'w', 'utf-8')
+    for tem in a_tem.values():
+        if tem in tems:
+            count += 1
+        else:
+            fw.write(tem+'\n')
+    fw.close()
+
+    logging.info("Template: %d"%len(a_tem.values()))
+    logging.info("Hit: %d"%count))
+    logging.info("Template coverage: %f\n"%(count*1.0/len(a_tem.values())))
+
+def infobox_coverage(tem_triple, a_tem, a_infobox):
+
+    tem_attribute = {}
+    for a, infobox in a_infobox.iteritems():
+        tem = a_tem[a]
+        if not tem in tem_attribute:
+            tem_attribute[tem] = set()
+        for k in infobox:
+            tem_attribute[tem].add(k)
+
+    total = hit = 0
+    for tem, attrs in tem_attribute.iteritems():
+        total += len(attrs)
+        if tem in tem_triple:
+            for a in attrs:
+                if a in tem_triple[tem]:
+                    hit += 1
+
+    logging.info("Total: %d"%total)
+    logging.info("Hit: %d"%hit)
+    logging.info("Infobox coverage: %f\n"%(hit*1.0/total))
+
+def infobox_replace(tem_triple, a_tem, a_infobox):
+    pass
+    
+
+if __name__=="__main__":
+    DIR = "/data/xlore20160223"
+    #tem_triple = read_template_triple(os.path.join(DIR, "Template/enwiki-20160305-template-triple.dat"))
+    #tem_redirect = read_redirect_template(os.path.join(DIR, "Template/enwiki-template-redirect.dat"))
+    #a_tem, a_infobox = read_wiki_infobox(os.path.join(DIR, "wikiExtractResult/enwiki-infobox-tmp.dat"))
+    tem_triple = read_template_triple(os.path.join(DIR, "Template/zhwiki-20160203-template-triple.dat"))
+    tem_redirect = read_redirect_template(os.path.join(DIR, "Template/zhwiki-template-redirect.dat"))
+    a_tem, a_infobox = read_wiki_infobox(os.path.join(DIR, "wikiExtractResult/zhwiki-infobox-tmp.dat"))
+    a_tem = template_replace(a_tem, tem_redirect, a_tem.values())
+    template_coverage(tem_triple, a_tem, tem_redirect)
+    infobox_coverage(tem_triple, a_tem, a_infobox)
+    
